b00c6ce415837335c70d72a8056343178ac3f63d
chushanshan520 <1545173163@qq.com>
2019-11-15 14:07:48 +0800

    这是花市网终极版2

--- huashi/js/login.js
+++ huashi/js/login.js
@@ -4,3 +4,41 @@ $("#login").children(".margin").children(".right").children(".ul1").children("li
 		
         $(".right").children("div").css("display","none").eq($(this).index()).css("display","block");
 })
+
+class login{
+		constructor() {
+		   this.idname=document.getElementsByClassName("idname")[0];
+		   this.idword=document.getElementsByClassName("password")[0];
+		   this.log=getCookie("log") ? JSON.parse(getCookie("log")) : [];
+		   this.idsub=document.getElementsByClassName("sub")[0];
+		   this.check();
+		}
+		
+		check(){
+			var that=this;
+			this.idsub.onclick=function(){
+				that.getcoo();
+			}
+		}
+		
+		getcoo(){
+			var log=JSON.parse(getCookie("log"));
+			for(var i=0;i<log.length;i++){
+				if(log[i].name!=this.idname.value){
+					alert("未注册，去注册");
+				}
+				if(log[i].name==this.idname.value && log[i].pass!=this.idword.value){
+					alert("密码不对");
+				}
+				if(log[i].name==this.idname.value && log[i].pass==this.idword.value){
+					this.go();
+					break;
+				}
+			}
+		}
+		go() {
+		    location.href ="../html/index.html";
+		}
+	}
+	
+	new login();
--- huashi/js/register.js
+++ huashi/js/register.js
@@ -5,6 +5,51 @@ $("#register").children(".margin").children(".cen").children(".ul1").children("l
         $(".cen").children("div").css("display","none").eq($(this).index()).css("display","block");
 })
 
-
+class regist{
+		constructor() {
+		    this.username=document.getElementsByClassName("username")[0];
+			this.password=document.getElementsByClassName("password")[0];
+			this.password1=document.getElementsByClassName("password1")[0];
+			this.checkbox=document.getElementsByClassName("checkbox")[0];
+			this.email=document.getElementsByClassName("email")[0];
+			this.log=getCookie("log") ? JSON.parse(getCookie("log")) : [];
+			this.sub=document.getElementsByClassName("sub")[0];
+			this.reg();
+		}
+		
+		reg(){
+			var that=this;
+			this.sub.onclick=function(){
+				that.checkUser();
+				
+			}
+		}
+		checkUser(){
+			var user=this.username.value; 
+			var pass=this.password.value;
+			var pass1=this.password1.value;
+			var emi=this.email.value;
+			var reg1=/^[a-zA-Z][a-zA-Z0-9]{3,15}$/;
+			var reg2=/^[a-zA-Z0-9]{4,10}$/;
+			var reg3=/^\w+@\w+\.(com)$|(cn)$/;
+			if(reg1.test(user) && reg2.test(pass) && reg3.test(emi) && pass==pass1 && this.checkbox.checked==true){
+					alert("注册成功");
+					this.setcoo();
+				}else{
+					alert("注册失败");
+				}
+			}
+		
+		setcoo(){
+					this.log.push({
+						name:this.username.value,
+						pass:this.password.value
+						
+					})
+					setCookie("log",JSON.stringify(this.log));
+		}
+		
+	}
+new regist();
 
 
